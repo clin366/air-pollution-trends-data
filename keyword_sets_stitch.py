649b410f662bd41c182f3b23ad47623f546d11a5
elviskahoro <ekk0809@gmail.com>
2019-07-01 12:55:33 -0400

    Cleaned up code. Removed unused lines. Added more intuitive print statements

--- keyword_sets_stitch.py
+++ keyword_sets_stitch.py
@@ -22,12 +22,10 @@ cols.remove('date')
 
 is_first_run = True
 first_slice_index = None
-last_slice_index = None
 list_of_stitched_time_ranges = list()
 scale = pd.DataFrame(index=cols)
 scale['scale'] = 1
 
-# creates a list of all the months in our data's time range
 # assumes the list of dates is the first column
 first_date_in_table = trends.iloc[0, 0]
 last_date_in_table = trends.iloc[-1, 0]
@@ -37,6 +35,7 @@ for last_datetime_in_month in months_in_table:
     if last_datetime_in_month.month in STITCHING_MONTHS:
         first_day_in_month = datetime(last_datetime_in_month.year, last_datetime_in_month.month, 1).strftime("%Y-%m-%d")
         last_day_in_month = last_datetime_in_month.strftime("%Y-%m-%d")
+        print("\n" + str(last_day_in_month))
 
         # list of indices duplicate start and end dates for current month
         duplicate_first_day_in_month_list = np.where(trends["date"] == first_day_in_month)[0]
@@ -47,34 +46,31 @@ for last_datetime_in_month in months_in_table:
             continue
         else:
             # creates a df with first 6 months of original data
-            last_slice_index = duplicate_last_day_in_month_list[0]
-            time_range = trends.iloc[first_slice_index: last_slice_index + 1]
+            time_range = trends.iloc[first_slice_index: duplicate_last_day_in_month_list[0] + 1]
             time_range = time_range.set_index('date')
 
+            print(scale)
             # scales each column
-            for kw in cols:
-                time_range[kw] = time_range[kw].apply(lambda x: x * scale.loc[kw])
+            if not is_first_run:  # scale is 1 for first run
+                for kw in cols:
+                    time_range[kw] = time_range[kw].apply(lambda x: x * scale.loc[kw])
 
             list_of_stitched_time_ranges.append(time_range)
 
-            stitched_time_range_table = None
+            if len(duplicate_first_day_in_month_list) == 1 or len(duplicate_last_day_in_month_list) == 1:
+                break
 
             if is_first_run:
                 is_first_run = False
-                past_avg_month_table_start = duplicate_first_day_in_month_list[0]
-                past_avg_month_table_end = duplicate_last_day_in_month_list[0]
-                past_avg = trends.iloc[past_avg_month_table_start: past_avg_month_table_end].replace(0, pd.np.NaN).mean(axis=0)
+                past_avg = trends.iloc[duplicate_first_day_in_month_list[0]: duplicate_last_day_in_month_list[0]+1].replace(0, pd.np.NaN).mean(axis=0)
             else:
                 past_avg = time_range.loc[first_day_in_month: last_day_in_month].replace(0, pd.np.NaN).mean(axis=0)
 
-            future_avg = trends.iloc[duplicate_first_day_in_month_list[1]: duplicate_last_day_in_month_list[1]].replace(0, pd.np.NaN).mean(axis=0)
+            future_avg = trends.iloc[duplicate_first_day_in_month_list[1]: duplicate_last_day_in_month_list[1] + 1].replace(0, pd.np.NaN).mean(axis=0)
             past_avg = past_avg.fillna(1)
             future_avg = future_avg.fillna(1)
             scale = future_avg / past_avg
-
             first_slice_index = duplicate_last_day_in_month_list[1] + 1
-            if first_slice_index >= len(trends.index):
-                break
 
 aggregated_monthly_tables = pd.concat(list_of_stitched_time_ranges, sort=True)
 aggregated_monthly_tables.to_csv(input_filename.replace('.csv', '_stitched.csv'))
